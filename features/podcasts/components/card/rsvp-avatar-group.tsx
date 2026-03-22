225e79f7efa7d284acea1e997f7e68ca800ac591
2gbeh <etugbeh@outlook.com>
2026-03-22 09:50:19 +0100

    feat: update RsvpAvatarGroup fetcher

--- features/podcasts/components/card/rsvp-avatar-group.tsx
+++ features/podcasts/components/card/rsvp-avatar-group.tsx
@@ -8,17 +8,23 @@ import {
 } from "@/components/shadcn/ui/avatar";
 import { AvatarGroup } from "@/components/atoms/avatar-builder";
 import { getListenersCountAction } from "@/lib/supabase/services/listeners/actions/getListenersAction";
+import { PodcastHelper } from "@/lib/supabase/services/podcasts/helper";
 
 interface Props {
   podcast_id: number;
 }
 
 export const RsvpAvatarGroup = ({ podcast_id }: Props) => {
-  const [totalSafe, setTotalSafe] = useState(5);
+  const [total, setTotal] = useState(1);
 
   async function fetcher() {
-    const { data: total } = await getListenersCountAction({ podcast_id });
-    setTotalSafe((total || 0) + 5);
+    const listeners = PodcastHelper.GetPageItem(podcast_id).listeners;
+    if (listeners) {
+      setTotal(listeners);
+    } else {
+      const { data: total } = await getListenersCountAction({ podcast_id });
+      setTotal(total || 1);
+    }
   }
 
   useEffect(() => {
@@ -26,7 +32,7 @@ export const RsvpAvatarGroup = ({ podcast_id }: Props) => {
   }, []);
   //
   return (
-    <AvatarGroup count={totalSafe - 3}>
+    <AvatarGroup count={total}>
       {[
         {
           src: "/images/icon-hwp-labs.png",
