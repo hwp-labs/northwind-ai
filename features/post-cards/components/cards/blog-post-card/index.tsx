f347981412b76437f51bcec87fbf8f287c66ac27
2gbeh <etugbeh@outlook.com>
2026-04-04 21:50:44 +0100

    feat: compress blog images

--- features/post-cards/components/cards/blog-post-card/index.tsx
+++ features/post-cards/components/cards/blog-post-card/index.tsx
@@ -1,3 +1,7 @@
+import clsx from "clsx";
+import { FaGlobeAfrica } from "react-icons/fa";
+//
+import { Badge } from "@/components/shadcn/ui/badge";
 import { AvatarGroup } from "@/components/atoms/avatar-builder";
 import { Avatar, AvatarImage } from "@/components/shadcn/ui/avatar";
 import { momentUtil } from "@/utils/moment-util";
@@ -7,11 +11,13 @@ import { CardBuilder } from "../../card-builder";
 import data from "./data.json";
 
 interface Data {
-  icons: string[];
+  classNames?: { img?: string; h1?: string };
   thumbnail: string;
+  categories: string[];
   headline: string;
   location: string;
   date: string;
+  icons: string[];
 }
 
 interface Props {
@@ -41,21 +47,38 @@ export const BlogPostCard = ({ page = 1 }: Props) => {
           src={`/uploads/blog/${item.thumbnail}`}
           alt=""
           width={640}
-          className="absolute inset-0 size-full object-cover"
+          className={clsx(
+            "absolute inset-0 size-full object-cover",
+            item.classNames?.img,
+          )}
         />
-        <footer className="bg-background/90 absolute bottom-8 w-full px-8 py-6 font-[Montserrat] text-white">
+        <footer className="bg-background/90 absolute bottom-8 w-full px-8 py-4 font-[Montserrat] text-white">
           <div className="flex-row-cs gap-1 text-sm font-medium">
             <time dateTime={item.date}>{momentUtil.shortDate(item.date)}</time>
             &bull;
             <address>{item.location}</address>
           </div>
-          <p className="tracking-wide_ mt-2 font-[Raleway] text-[20px] font-bold">
+          <h1
+            className={clsx(
+              "mt-2 font-[Raleway] text-[20px] font-bold",
+              item.classNames?.h1,
+            )}
+          >
             {item.headline}
-          </p>
-          <p className="mt-4 space-x-2 text-xs font-medium tracking-wide">
-            <span className="text-contrast">&gt;_</span>
-            <span>{APP.domain}</span>
-          </p>
+          </h1>
+          <section className="flex-row-cb mt-4">
+            <div className="flex-row-cs gap-2">
+              {item.categories.map((v, i) => (
+                <Badge key={i} variant="secondary">
+                  {v}
+                </Badge>
+              ))}
+            </div>
+            <div className="flex-row-cs gap-1.5 text-xs font-medium tracking-wide">
+              <FaGlobeAfrica className="text-contrast"/>
+              <span>{APP.domain}</span>
+            </div>
+          </section>
         </footer>
       </main>
     </>
