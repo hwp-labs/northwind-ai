38e2ab1ebe2a957a479f85371834f95c0fd1c4dd
2gbeh <etugbeh@outlook.com>
2026-05-23 21:02:30 +0100

    feat(podcast -app): update guests avatar

--- components/species/podcast-app/components/guests.tsx
+++ components/species/podcast-app/components/guests.tsx
@@ -1,23 +1,24 @@
 "use client";
 
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import { ListHeader } from "./list-header";
 
 export const Guests = () => {
+  const [randData, setRandData] = useState(data);
+  useEffect(() => {
+    setRandData((s) => [...s].sort(() => Math.random() - 0.5));
+  }, []);
+  //
   return (
     <section className="debug_ max-w-svw">
       <div className="px-5">
         <ListHeader>Featured Guests</ListHeader>
       </div>
       <ul className="scrollbar-hide gap-4_ mt-4 flex snap-x snap-mandatory items-end overflow-x-auto scroll-smooth pb-4">
-        {data.map((item, i) => (
+        {randData.map((item, i) => (
           <li key={i} className="border_ max-w-[100px] min-w-[100px]">
-            <figure
-              className="debug3_ flex-col-cc flex-shrink-0 snap-center cursor-pointer"
-              onClick={() =>
-                window.open(`https://x.com/${item.username}`, "_blank")
-              }
-            >
+            <figure className="debug3_ flex-col-cc flex-shrink-0 snap-center">
               <Image
                 src={item.src}
                 alt=""
@@ -25,7 +26,12 @@ export const Guests = () => {
                 height={56}
                 className="border-outline size-[56px] rounded-full border-2"
               />
-              <figcaption className="flex-col-cc mt-2 gap-0.5 truncate text-sm">
+              <figcaption
+                className="flex-col-cc debug_ mt-2 cursor-pointer gap-0.5 truncate text-sm"
+                onClick={() =>
+                  window.open(`https://x.com/${item.username}`, "_blank")
+                }
+              >
                 <strong className="text-white">{item.name}</strong>
                 {/* <small>{item.username}</small> */}
               </figcaption>
@@ -38,13 +44,38 @@ export const Guests = () => {
 };
 
 const data = [
-  // {
-  //   src: "/uploads/logos/grab.png",
-  //   name: "Grab",
-  //   username: "@InsideGrab",
-  // },
   {
-    src: "/uploads/logos/adiz.png",
+    src: "/uploads/podcast/avatar-polalere.png",
+    name: "Phillip O.",
+    username: "@philstring93",
+  },
+  {
+    src: "/uploads/podcast/avatar-aosawere.png",
+    name: "Anthony O.",
+    username: "@workofao",
+  },
+  {
+    src: "/images/avatar-etugbeh2.png",
+    name: "Emanuel T.",
+    username: "@2gbeh",
+  },
+  {
+    src: "/uploads/podcast/avatar-aisaac.jpg",
+    name: "Aderoju I.",
+    username: "@Aderoju_isaac9",
+  },
+  {
+    src: "/uploads/podcast/avatar-ceffect.jpg",
+    name: "Cody E.",
+    username: "@CodyEffect",
+  },
+  {
+    src: "/uploads/podcast/avatar-eafiemo.jpg",
+    name: "Edafe A.",
+    username: "@edafeafiemo",
+  },
+  {
+    src: "/uploads/podcast/avatar-iadebiyi.jpg",
     name: "Isaac A.",
     username: "@theisaacade",
   },
@@ -54,17 +85,17 @@ const data = [
     username: "@OluehAboyowa",
   },
   {
-    src: "/uploads/logos/izivote.png",
+    src: "/uploads/podcast/avatar-juyi.jpg",
     name: "Joshua U.",
     username: "@joshuaouyi",
   },
   {
-    src: "/uploads/logos/bookin.png",
+    src: "/uploads/podcast/avatar-oifeanyi.jpg",
     name: " Oluchi I.",
     username: "@feanyluch",
   },
   {
-    src: "/uploads/logos/scupex.png",
+    src: "/uploads/podcast/avatar-makinbo.jpg",
     name: "Martins A.",
     username: "@sanmiAkinbo",
   },
